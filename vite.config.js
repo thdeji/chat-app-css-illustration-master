90e33b68c89fabc5b6789da505160f94edabca5d
thdeji <omotobaadedeji@gmail.com>
2026-01-06 22:45:03 +0100

    setting up ghpages

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base:'/chat-app-css-illustration-master/',
+  plugins: [
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
+})
