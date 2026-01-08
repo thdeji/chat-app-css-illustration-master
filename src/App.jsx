dc061623a4fe4bdc2a3e3db4ae2f7c90bae5260e
thdeji <omotobaadedeji@gmail.com>
2026-01-08 01:36:26 +0100

    fix responsiveness on md screens

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <div className="absolute bg-linear-to-t  from-Purple500 via-Purple600 to-Purple600 h-[50%] md:h-[120%] md:w-[35%] w-[50%] top-0 left-0 rounded-br-full md:-left-35 md:-top-50 md:rounded-full"></div>
       <PhoneInterface />
       <div className="z-10 font-Rubik md:text-left text-center mb-15">
-        <h1 className="text-3xl md:text-5xl font-semibold text-Purple950 mb-5">Simple booking</h1>
+        <h1 className="text-3xl md:text-5xl font-semibold text-Purple950 mb-5 text-nowrap">Simple booking</h1>
         <p className="mx-15 md:mx-0 text-Gray300 md:leading-loose">
         Stay in touch with our dog walkers through the chat interface. This makes it easy to 
   discuss arrangements and make bookings. Once the walk has been completed you can rate 
